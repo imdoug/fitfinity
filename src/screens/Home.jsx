8f6553d942fdfc2f870bcaf4879f14e5d32d1e81
imdoug <imdoug@icloud.com>
2023-02-20 13:48:12 -0500

    HomeBanner

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,9 +3,9 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
-// import Exercises from '../components/Exercises';
-// import SearchExercises from '../components/SearchExercises';
-// import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
@@ -13,9 +13,9 @@ const Home = () => {
 
   return (
     <Box>
-      {/* <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} /> */}
+       <HeroBanner />
+       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };
